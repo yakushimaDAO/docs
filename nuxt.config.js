470a54ada8d2babfec16331048b358e4e2f50245
warenghem <kevin.brosseau@warenghem.com>
2021-10-27 23:40:42 +0200

    add documentation

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+import theme from "@nuxt/content-theme-docs";
+
+export default theme({
+  /*
+   ** You can extend the nuxt configuration
+   ** Doc: https://content.nuxtjs.org/themes-docs#nuxtconfigjs
+   */
+  content: {
+    // Disable for security reason on CodeSandBox
+    liveEdit: false
+  }
+});
